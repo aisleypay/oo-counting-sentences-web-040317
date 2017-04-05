03c07a726a3d8d84464db478356cf469a4ca8562
github username <github email address>
2017-04-05 16:31:53 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    punctuation = ["?", "!", "."]
+    final_sentence = []
+    answer = ""
 
+    self.reverse.each_char { |chr|
+      if punctuation.include?(chr) == true
+        answer += chr
+      else
+        final_sentence << answer if answer != ""
+        answer = ""
+      end
+
+      # if (punctuation.include?(chr)) && (punctuation.include?(chr[index + 1]) == false)
+      #   counter += 1
+      # end
+    }
+
+    final_sentence.length
   end
 end
